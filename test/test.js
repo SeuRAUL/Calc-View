7ef5a201847545ebf12c108b47ffddcddb02c124
SeuRAUL <kenny.is.inmortal@gmail.com>
2014-06-06 14:23:30 -0300

    test

---
+++ test/test.js
@@ -0,0 +1,16 @@
+describle('Calculator', function () {
+	describle('app', function() {
+		beforeEach(function() {
+			this.$scope = {};
+			this.controller = new app(this.$scope);
+		});
+
+		it('deve calcular 3', function() {
+			this.$scope.number1 = 1;
+			this.$scope.number2 = 2;
+			this.$scope.operation = 'add';
+
+			expect(this.$scope.result).toBe(3);
+		})
+	})
+})
