a533962e9d360a872909b937bf4ee3f9d2a79aa8
SeuRAUL <kenny.is.inmortal@gmail.com>
2014-06-06 03:00:15 -0300

    view e controller

---
+++ js/app.js
@@ -0,0 +1,43 @@
+var calculator = angular.module('Calculator', []);
+calculator.controller('Calculator', function ($scope, $http) {
+	var number1, number2, operation;
+
+	var sendCalculationRequest = function(formData) {
+		$http.post('http://calc-server.herokuapp.com/calc/calcule', formData)
+			.success(function(data) {
+				//$scope.calculation = data;
+				$scope.result = data;
+				/*number1 = data;
+				number2 = undefined;
+				operation = undefined;*/
+
+			}) 
+			.error(function(data) {
+				console.log('Error: '+ data);
+			});
+	};
+
+	$scope.result = '_';
+    $scope.calculation;
+    $scope.operation = 'add';
+
+    $scope.erase = function() {
+    	$scope.result = '_';
+	    $scope.calculation;
+	    $scope.number1 = null;
+	    $scope.number2 = null;
+    }
+
+    $scope.calculate = function() {
+    	if($scope.number1 && $scope.number2) {
+    		var formData = { operation: $scope.operation, number1: $scope.number1, number2: $scope.number2};
+
+    		if($scope.number2 === 0 && $scope.operation === 'div') {
+    			alert('Invalid Operation');
+    		}
+
+    		sendCalculationRequest(formData);
+    	}
+    };
+
+})
